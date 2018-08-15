82082773980cf709428f7e001b5c5b42fb3a395f
Adrian <adi.bal74@gmail.com>
2018-08-15 14:30:12 +0200

    added random sensor values

--- clock.py
+++ clock.py
@@ -1,19 +1,18 @@
 from Tkinter import *
 import time
-import Adafruit_DHT
+import random
 root = Tk()
-root.attributes('-fullscreen', True)
-root.geometry('800x600')
+root.geometry('800x480')
 time1 = ''
-humidity = 0
-temperatur = 0
-clock = Label(root, font=('courier', 64, 'bold'), bg='black', fg="blue")
-clock.pack(fill=BOTH, expand=1)
-sensor_text = Text(root, height=4, width=18, font='courierbold 42', background="#000000", fg="white")
-sensor_text.insert(END, "T: \n")
-sensor_text.insert(END, "H: \n")
-sensor_text.insert(END, "B: \n")
-sensor_text.pack(fill=BOTH, expand=1)
+clock = Label(root, font=('arial', 60, 'bold'), bg='black', fg="blue")
+clock.pack(fill=BOTH)
+temp_display = Label(root, font=('arial', 42, 'bold'), bg='white', fg="blue")
+humidity_display = Label(root, font=('arial', 42, 'bold'), bg='white', fg="blue")
+baro_display = Label(root, font=('arial', 42, 'bold'), bg='white', fg="blue")
+temp_display.pack(side= LEFT)
+humidity_display.pack(side = RIGHT)
+baro_display.pack(side = LEFT)
+
 def tick():
     global time1
     # get the current local time from the PC
@@ -26,12 +25,18 @@ def tick():
         # to update the time display as needed
         # could use >200 ms, but display gets jerky
     clock.after(200, tick)
-def read_sensor():
-    global temperatur, humidity
-    humidity, temperatur = Adafruit_DHT_read(Adafruit_DHT22, 4)
-    sensor_text.insert(END, "T: %s\n" % temperatur)
-    sensor_text.insert(END, "H: %s \n" % humidity)
-    
+def get_temp_value():
+    temp_value = random.randrange(0,101,2)
+    temp_display.config(text=temp_value)
+    temp_display.after(2000, get_temp_value)
+def get_humidity_value():
+    humidity_value = random.randrange(0,101,2)
+    humidity_display.config(text=humidity_value)
+    humidity_display.after(2000, get_humidity_value)
 
 tick()
+get_temp_value()
+get_humidity_value()
 root.mainloop()
+
+
