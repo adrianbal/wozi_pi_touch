75dbe02280151c336b7187d77f7defa0b0a9a3eb
adrianbal <adi.bal74@gmail.com>
2018-12-18 08:51:18 +0100

    added BME680 sensor

--- clock.py
+++ clock.py
@@ -2,23 +2,31 @@ from Tkinter import *
 import time
 import random
 import sys
+import bme680
 def quit_program():
-    sys.exit() 
+    sys.exit()
+# Bosch Sensor 
+sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
+sensor.set_humidity_oversample(bme680.OS_2X)
+sensor.set_pressure_oversample(bme680.OS_4X)
+sensor.set_temperature_oversample(bme680.OS_8X)
+sensor.set_filter(bme680.FILTER_SIZE_3)
+
 root = Tk()
 root.attributes('-fullscreen', True)
 root.geometry('800x480')
 root.configure(background='black')
 time1 = ''
-clock = Label(root, font=('arial', 60, 'bold'), bg='black', fg="blue")
+
+clock = Label(root, font=('arial', 82, 'bold'), bg='black', fg="blue")
 clock.pack(side=TOP, fill=BOTH)
-temp_display = Label(root, font=('arial', 42, 'bold'), bg='black', fg="blue")
-humidity_display = Label(root, font=('arial', 42, 'bold'), bg='black', fg="blue")
-baro_display = Label(root, font=('arial', 42, 'bold'), bg='black', fg="blue")
-temp_display.pack(side= TOP, fill=BOTH)
-humidity_display.pack(side = TOP, fill=BOTH)
-baro_display.pack(side = TOP, fill=BOTH)
+
 quitButton = Button(text="Quit", command=quit_program)
 quitButton.pack(side = BOTTOM)
+
+temp_display = Label(root, font=('arial', 42, 'bold'), bg='black', fg="green")
+temp_display.pack(side= BOTTOM, fill=BOTH)
+
 def tick():
     global time1
     # get the current local time from the PC
@@ -32,17 +40,16 @@ def tick():
         # could use >200 ms, but display gets jerky
     clock.after(200, tick)
 def get_temp_value():
-    temp_value = random.randrange(0,101,2)
-    temp_display.config(text=temp_value)
-    temp_display.after(2000, get_temp_value)
-def get_humidity_value():
-    humidity_value = random.randrange(0,101,2)
-    humidity_display.config(text=humidity_value)
-    humidity_display.after(2000, get_humidity_value)
+    sensor.get_sensor_data()
+    output = '{0:.1f} C {1:.1f} hPa {2:.1f} %RH'.format(
+                sensor.data.temperature,
+                sensor.data.pressure,
+                sensor.data.humidity)
+    temp_display.config(text=output)
+    temp_display.after(4000, get_temp_value)
 
 tick()
 get_temp_value()
-get_humidity_value()
 root.mainloop()
 
 
